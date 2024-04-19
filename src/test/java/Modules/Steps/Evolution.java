ae08d32560803c547dc6a4e421947dacbd3e7332
SoftwareControl101 <scautomation@leekie.com>
2024-04-19 11:30:10 +0800

    Trial of New REPORT

--- src/test/java/Modules/Steps/Evolution.java
+++ src/test/java/Modules/Steps/Evolution.java
@@ -8,9 +8,12 @@ import Utilities.Listeners.Events;
 import Utilities.Listeners.FileEvent;
 import Utilities.Listeners.JsonGenerator;
 import Utilities.Objects.Component;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import engine.Driver;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import java.io.IOException;
 import java.util.*;
@@ -118,5 +121,104 @@ public class Evolution extends Driver {
     @And("Display JSON {string}")
     public void displayJSON(String provider) throws IOException {
         JsonFormatter.generate(information, provider);
+    }
+
+
+
+
+    // ? ====================================================================
+    // ? ====================================================================
+    // ? ====================================================================
+    // ? ====================================================================
+
+
+    @When("Try to get All {string} Tables of Evolution")
+    public void tryToGetAllTablesOfEvolution(String category) throws IOException {
+
+        provider = "Evolution";
+        String activeCategory = Events.getText(EvolutionComponents.navigation);
+
+        Component component = switch (category){
+
+            case "Roulette" -> EvolutionComponents.Navigation.roulette;
+            case "Baccarat & Sic Bo" -> EvolutionComponents.Navigation.baccarat_SicBo;
+            case "Poker" -> EvolutionComponents.Navigation.poker;
+            default -> EvolutionComponents.Navigation.gameShows;
+
+        };
+
+        if(!activeCategory.equals(category)){
+
+            Waiting.fewSeconds(2);
+            Events.click(EvolutionComponents.logo);
+
+            Waiting.fewSeconds(2);
+            Events.click(EvolutionComponents.games);
+
+            Waiting.fewSeconds(2);
+            Events.click(component);
+        }
+
+        FileEvent.readExcel(category, provider);
+        tableList = FileEvent.gameList;
+
+        System.out.println("===============");
+        System.out.println("Excel File Table List");
+        System.out.println("Table Size: " + tableList.size());
+        System.out.println("===============");
+        for(String table : tableList){
+
+            System.out.println(table);
+        }
+        System.out.println("============================================================");
+        System.out.println("============================================================");
+
+    }
+
+    @Then("Verify {string} of Provider Evolution")
+    public void verifyOfProviderEvolution(String category) throws IOException {
+
+        fileList = new ArrayList<>();
+        lobbyList = new ArrayList<>();
+
+        switch (category){
+
+            case "Baccarat & Sic Bo" -> product = "Baccarat & Sic Bo";
+            case "Roulette" -> product = "Roulette";
+            case "Poker" -> product = "Poker";
+            default -> product = "Game Shows";
+        }
+
+        EvolutionMethods.verify(category);
+
+        System.out.println("=======================");
+        System.out.println("Left Join Verification");
+        for(String table : EvolutionMethods.tableList){
+
+            if(!tableList.contains(table)){
+
+                System.out.println("Excel List have no: " + table);
+                fileList.add(table);
+            }
+        }
+
+        System.out.println("=======================");
+        System.out.println("Right Join Verification");
+        for(String table : tableList){
+
+            if(!EvolutionMethods.tableList.contains(table)){
+
+                System.out.println("Game Lobby have no: " + table);
+                lobbyList.add(table);
+            }
+        }
+
+
+        if(!fileList.isEmpty() || !lobbyList.isEmpty()){
+
+            information.add(JsonGenerator.generate(product, tableList, fileList, lobbyList));
+            JsonFormatter.generate(information, provider);
+
+        }
     }
 }
--- src/test/resources/Features/Priority1/Evolution.feature
+++ src/test/resources/Features/Priority1/Evolution.feature
@@ -19,3 +19,17 @@ Feature: Checking Table List in Evolution Casino
     And Verify "Game Shows" of Evolution
 
     And Display JSON "Evolution"
+
+  Scenario: Checking all games of Evolution
+
+    When Try to get All "Baccarat & Sic Bo" Tables of Evolution
+    Then Verify "Baccarat & Sic Bo" of Provider Evolution
+
+    When Try to get All "Roulette" Tables of Evolution
+    Then Verify "Roulette" of Provider Evolution
+
+    When Try to get All "Poker" Tables of Evolution
+    Then Verify "Poker" of Provider Evolution
+
+    When Try to get All "Game Shows" Tables of Evolution
+    Then Verify "Game Shows" of Provider Evolution
